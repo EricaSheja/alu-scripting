1498da0e04c2ae50964bf96edf980ee0e3e4c227
kguido798 <g.kayigamba@alustudent.com>
2026-02-18 12:08:06 +0200

    aa

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-'''
-    This module contains the function top_ten
-'''
+"""
+1-top_ten
+Prints the titles of the first 10 hot posts
+for a given subreddit.
+"""
+
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        Returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
-        print(None)
+    """Print top 10 hot post titles of a subreddit."""
+    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
+    headers = {"User-Agent": "alu-scripting-app"}
+    params = {"limit": 10}
 
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
 
-if __name__ == "__main__":
-    top_ten(argv[1])
+    if response.status_code != 200:
+        print("None")
+        return
+
+    posts = response.json().get("data").get("children")
+
+    if not posts:
+        print("None")
+        return
+
+    for post in posts:
+        print(post.get("data").get("title"))
