da826c169b2d99591995f0a7d543b0c3372c7992
kguido798 <g.kayigamba@alustudent.com>
2026-02-18 12:06:25 +0200

    mm

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-    this module contains the function top_ten
+    This module contains the function top_ten
 '''
 import requests
 from sys import argv
@@ -8,7 +8,7 @@ from sys import argv
 
 def top_ten(subreddit):
     '''
-        returns the top ten posts for a given subreddit
+        Returns the top ten posts for a given subreddit
     '''
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
