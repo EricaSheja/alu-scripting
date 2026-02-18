dc2c8055a9e8f6bb9e82c5fddc6e5d62663201cf
kguido798 <g.kayigamba@alustudent.com>
2026-02-18 12:11:39 +0200

    cc

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-1-top_ten
-Prints the titles of the first 10 hot posts
-for a given subreddit.
-"""
-
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Print top 10 hot post titles of a subreddit."""
-    url = "https://api.reddit.com/r/{}/hot".format(subreddit)
-    headers = {"User-Agent": "alu-scripting"}
-
-    response = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-
-    if response.status_code != 200:
-        print("None")
-        return
-
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
     try:
-        posts = response.json().get("data").get("children")
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
     except Exception:
-        print("None")
-        return
+        print(None)
 
-    if posts is None:
-        print("None")
-        return
 
-    for post in posts[:10]:
-        print(post.get("data").get("title"))
+if __name__ == "__main__":
+    top_ten(argv[1])
